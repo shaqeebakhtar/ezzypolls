267a376aee69afcfc1e37bef111ef2f5c4e0ff4b
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-12-07 02:12:34 +0530

    add clustering for scale

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import cors from 'cors';
 import express, { Express } from 'express';
 import { router } from './routes';
 import { PORT } from './utils/config';
+import cluster from 'cluster';
+import { cpus } from 'os';
 
 const app: Express = express();
 
 const port = PORT || 3000;
+const cpuCount = cpus().length;
 
 const corsOption = {
   origin: 'http://localhost:5173',
@@ -16,6 +19,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+if (cluster.isPrimary) {
+  console.log(`Master process ${process.pid} is running`);
+
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker process ${worker.process.pid} exited. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  app.listen(port, () => {
+    console.log(
+      `Worker process ${process.pid} is running at http://localhost:${port}`
+    );
+  });
+}
